33399145d91a8c04322e88228fe834dfa6a48a4f
Keyvan Fatehi <keyvanfatehi@gmail.com>
2017-02-14 13:07:05 -0800

    pics

--- public/js/radial-menu.js
+++ public/js/radial-menu.js
@@ -8,30 +8,42 @@ var height = innerHeight;
 var radius = 175;
 var donutWidth = 75;
 var color = d3.scaleOrdinal(d3.schemeCategory20b);
-var svg = d3.select('#chart')
-  .append('svg')
-  .attr('width', width)
+var svg = d3.select('#chart').append('svg')
+
+var defs =  svg.append("defs");
+
+var g = svg.attr('width', width)
   .attr('height', height)
   .append('g')
   .attr('transform', 'translate(' + (width / 2) + ',' + (height / 2) + ')');
+
 var arc = d3.arc()
   .innerRadius(radius - donutWidth)
   .outerRadius(radius*8);
-var pie = d3.pie()
-  .value(function(d) { return d.count; })
-  .sort(null);
+
+var pie = d3.pie().value(1);
+
+
 d3.csv('topics.csv', function(error, dataset) {
-  dataset.forEach(function(d) {
-    d.count = +d.count;
-    d.enabled = true;                                         // NEW
+
+  dataset.forEach(function(item) {
+    var height = innerHeight/2+100;
+    var width = innerWidth/2+100;
+      defs.append("pattern")
+      .attr("id", item.id)
+      .attr('patternUnits', 'userSpaceOnUse')
+      .attr('width', width)
+      .attr('height', height)
+      .append("image")
+      .attr("xlink:href", item.img)
+      .attr('width', width+100)
+      .attr('height', height+100)
   });
-  var path = svg.selectAll('path')
+
+  var path = g.selectAll('path')
     .data(pie(dataset))
     .enter()
     .append('path')
     .attr('d', arc)
-    .attr('fill', function(d, i) {
-      return color(d.data.label);
-    })                                                        // UPDATED (removed semicolon)
-    .each(function(d) { this._current = d; });                // NEW
+    .attr("fill", (d)=>`url(#${d.data.id})`)
 });
