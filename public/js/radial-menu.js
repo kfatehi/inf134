6c2cb0ee07e70b0cfc50760c5a9ff00cb6b5419a
Keyvan Fatehi <keyvanfatehi@gmail.com>
2017-02-17 12:33:39 -0800

    menu resize

--- public/js/radial-menu.js
+++ public/js/radial-menu.js
@@ -3,28 +3,29 @@
 // pattern fill
 // http://stackoverflow.com/questions/25881186/d3-fill-shape-with-image-using-pattern
 
-var width = innerWidth;
-var height = innerHeight;
-var radius = 175;
-var donutWidth = 75;
-var color = d3.scaleOrdinal(d3.schemeCategory20b);
-var svg = d3.select('#chart').append('svg')
+function render(dataset) {
+  var width = innerWidth;
+  var height = innerHeight;
+  var radius = 175;
+  var donutWidth = 75;
+  var color = d3.scaleOrdinal(d3.schemeCategory20b);
 
-var defs =  svg.append("defs");
+  d3.select('#chart').selectAll('svg').remove();
 
-var g = svg.attr('width', width)
-  .attr('height', height)
-  .append('g')
-  .attr('transform', 'translate(' + (width / 2) + ',' + (height / 2) + ')');
+  var svg = d3.select('#chart').append('svg')
 
-var arc = d3.arc()
-  .innerRadius(radius - donutWidth)
-  .outerRadius(radius*8);
+  var defs =  svg.append("defs");
 
-var pie = d3.pie().value(1);
+  var g = svg.attr('width', width)
+    .attr('height', height)
+    .append('g')
+    .attr('transform', 'translate(' + (width / 2) + ',' + (height / 2) + ')');
 
+  var arc = d3.arc()
+    .innerRadius(radius - donutWidth)
+    .outerRadius(radius*8);
 
-d3.csv('topics.csv', function(error, dataset) {
+  var pie = d3.pie().value(1);
 
   dataset.forEach(function(item) {
     var height = innerHeight/2+100;
@@ -50,4 +51,10 @@ d3.csv('topics.csv', function(error, dataset) {
     .on('click', (d)=>{
       window.location = '/'+d.data.id;
     });
+}
+
+
+d3.csv('topics.csv', function(error, dataset) {
+  render(dataset);
+  d3.select(window).on('resize', ()=> render(dataset) );
 });
