b73f1556bed815db8a9f7f1589ab92c28f175f3b
Keyvan Fatehi <keyvanfatehi@gmail.com>
2017-02-17 14:53:18 -0800

    front page LGTM

--- index.js
+++ index.js
@@ -1,30 +1,20 @@
 var express = require('express');
 var app = express();
-var topics = [{
-  id: 'deforestation',
-  img: '/img/HomeDeforest.jpg',
-},{
-  id: 'pollution',
-  img: '/img/pollutionhome.jpg',
-},{
-  id: 'species-extinction',
-  img: '/img/SpeciesExtinction.jpg',
-},{
-  id: 'climate-change',
-  img: '/img/climatehome.jpg'
-}];
+var fs = require('fs');
+var parse = require('csv-parse/lib/sync');
+var csvFile = fs.readFileSync(__dirname+'/public/topics.csv');
+var topics = parse(csvFile, {columns: true});
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use('/js/lib', express.static(__dirname + '/node_modules'));
+app.use('/lib', express.static(__dirname + '/node_modules'));
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-  res.render('index', { topics });
+  res.render('index', { topics, donutSize: '100px' });
 });
 
 app.listen(app.get('port'), function() {
